958adcc8ce7ab89ae6ba48cd3057946166dce6b1
Adetunji <ashennaike@gmail.com>
2019-05-01 17:43:05 -0400

    completed the routes

--- routes/cohortRoute.js
+++ routes/cohortRoute.js
@@ -3,9 +3,9 @@ const express = require('express');
 const router = express.Router();
 
 //import database model
+const cohorts = require('../models/cohortModel');
 
-
-//error middleware
+//error returns
 const sendError = (msg, res) => {
   res.status(500).json({ error: `${msg}`});
 };
@@ -24,6 +24,106 @@ const nameCheck = () => {
 }
 
 //CRUD requests
+//get actions
+router.get('/', (req, res) => {
+  cohorts
+  .get()
+  .then( cohort => {
+    res.status(200).json({ cohort });
+  })
+  .catch( err => {
+    return sendError(err, res);
+  })
+})
+
+//get by id
+router.get('/:id', (req, res) => {
+  //set id
+  let ID = req.params.id
+
+  cohorts
+  .getById(ID)
+  .then( cohort => {
+    if(cohort === undefined){ 
+      return missingError(res) 
+    }
+    else{ 
+      return res.status(200).json({ cohort })
+    }
+  })
+  .catch( err => {
+    return sendError(err, res);
+  })
+})
+
+//post request
+router.post('/', (req, res) => {
+  //set req
+  const { name, cohort_id } = req.body;
+  const newcohort = { name, cohort_id };
+
+  //check req
+  if( !name || !cohort_id ) {
+    return newError(406, 'Provide a name and cohort id', res);
+  }
+  cohorts
+  .insert(newcohort)
+  .then( cohort => {
+    res.status(201).json({ cohort });
+  })
+  .catch( err => {
+    return sendError(err, res);
+  })
+})
+
+//update request
+router.put('/:id', (req, res) => {
+  //set id
+  let ID = req.params.id
+
+  //set req
+  const { name, cohort_id } = req.body;
+  const newcohort = { name, cohort_id };
+
+  //check req
+  if( !name || !cohort_id ) {
+    return newError(406, 'Provide a name and cohort id', res);
+  }
+  cohorts
+  .update(ID, newcohort)
+  .then( cohort => {
+    if(cohort === undefined){ 
+      return missingError(res) 
+    }
+    else{ 
+      res.status(202).json({ cohort });
+    }
+  })
+  .catch( err => {
+    return sendError(err, res);
+  })
+})
+
+//delete request 
+router.delete('/:id', (req, res) => {
+   //set id
+   let ID = req.params.id
+
+   cohorts
+   .remove(ID)
+   .then( cohort => {
+    if(cohort === undefined){ 
+      return missingError(res) 
+    }
+    else{ 
+      res.status(202).json({ cohort });
+    }
+   })
+   .catch( err => {
+     return sendError(err, res);
+   })
+})
+
 
 //export
 module.exports = router;
--- routes/studentRoute.js
+++ routes/studentRoute.js
@@ -24,6 +24,106 @@ const nameCheck = () => {
 }
 
 //CRUD requests
+//get actions
+router.get('/', (req, res) => {
+  students
+  .get()
+  .then( student => {
+    res.status(200).json({ student });
+  })
+  .catch( err => {
+    return sendError(err, res);
+  })
+})
+
+//get by id
+router.get('/:id', (req, res) => {
+  //set id
+  let ID = req.params.id
+
+  students
+  .getById(ID)
+  .then( student => {
+    if(student === undefined){ 
+      return missingError(res) 
+    }
+    else{ 
+      return res.status(200).json({ student })
+    }
+  })
+  .catch( err => {
+    return sendError(err, res);
+  })
+})
+
+//post request
+router.post('/', (req, res) => {
+  //set req
+  const { name, cohort_id } = req.body;
+  const newStudent = { name, cohort_id };
+
+  //check req
+  if( !name || !cohort_id ) {
+    return newError(406, 'Provide a name and cohort id', res);
+  }
+  students
+  .insert(newStudent)
+  .then( student => {
+    res.status(201).json({ student });
+  })
+  .catch( err => {
+    return sendError(err, res);
+  })
+})
+
+//update request
+router.put('/:id', (req, res) => {
+  //set id
+  let ID = req.params.id
+
+  //set req
+  const { name, cohort_id } = req.body;
+  const newStudent = { name, cohort_id };
+
+  //check req
+  if( !name || !cohort_id ) {
+    return newError(406, 'Provide a name and cohort id', res);
+  }
+  students
+  .update(ID, newStudent)
+  .then( student => {
+    if(student === undefined){ 
+      return missingError(res) 
+    }
+    else{ 
+      res.status(202).json({ student });
+    }
+  })
+  .catch( err => {
+    return sendError(err, res);
+  })
+})
+
+//delete request 
+router.delete('/:id', (req, res) => {
+   //set id
+   let ID = req.params.id
+
+   students
+   .remove(ID)
+   .then( student => {
+    if(student === undefined){ 
+      return missingError(res) 
+    }
+    else{ 
+      res.status(202).json({ student });
+    }
+   })
+   .catch( err => {
+     return sendError(err, res);
+   })
+})
+
 
 //export
 module.exports = router;
